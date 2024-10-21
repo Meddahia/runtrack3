cb2f6f4847612b09caf165b5fa40e0bb92498628
meddahia <fatimam.belghaouti@gmail.com>
2024-10-21 14:07:04 +0200

    premier commit

---
+++ jour01/job01/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+    <script>
+        window.onload =function()
+        {
+            alert("hello JavaScript !"); 
+        };
+    </script>
+</head>
+<body>
+    <h1>
+        Bienvenu sur ma page
+    </h1>
+    <p>
+        Cette page va afficher une alerte lorsque tu la charge
+    </p>
+
+</body>
+</html>
