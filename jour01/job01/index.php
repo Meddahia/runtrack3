5330360fd1f33a77d1f65d0f3db4685d776a0aca
meddahia <fatimam.belghaouti@gmail.com>
2024-10-21 14:52:14 +0200

    job02 src_script.js

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -5,13 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-
-    <script>
-        window.onload =function()
-        {
-            alert("hello JavaScript !"); 
-        };
-    </script>
+    <script src="script.js"></script>
 </head>
 <body>
     <h1>
